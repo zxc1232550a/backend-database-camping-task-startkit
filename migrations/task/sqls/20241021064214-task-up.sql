99241cf80f80a0c7c8dc59cccb319e0bd36fc61a
py_tsai <py_tsai@curdr.com>
2024-12-06 00:10:52 +0800

    finished 1-1 to 3-1

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -15,13 +15,38 @@
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
 
+INSERT INTO 
+	"USER" ("name", "email", "role")
+values
+	('李燕容', 'lee2000@hexschooltest.io', 'USER'),
+    ('王小明', 'wXlTq@hexschooltest.io', 'USER'),
+    ('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
+    ('好野人', 'richman@hexschooltest.io', 'USER'),
+    ('Q太郎', 'starplatinum@hexschooltest.io', 'USER'),
+    ('透明人', 'opcatiy0@hexschooltest.io', 'USER');
+
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
+
+UPDATE "USER"
+SET "role" = 'COACH'
+WHERE "email" IN ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io')
+AND "role" = 'USER';
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
 
+DELETE FROM "USER"
+WHERE "email" = 'opcatiy0@hexschooltest.io';
+
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 
+SELECT COUNT(*) AS "user_count"
+FROM "USER";
+
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
+
+SELECT *
+FROM "USER"
+LIMIT 3;
 
 
 --  ████████  █████   █    ████  
@@ -36,11 +61,39 @@
     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
 
+INSERT INTO 
+	"CREDIT_PACKAGE" ("name", "price", "credit_amount")
+VALUES
+    ('7 堂組合包方案', 1400, 7),
+    ('14 堂組合包方案', 2520, 14),
+    ('21 堂組合包方案', 4800, 21);
+
 -- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
     -- 1. `王小明` 購買 `14 堂組合包方案`
     -- 2. `王小明` 購買 `21 堂組合包方案`
     -- 3. `好野人` 購買 `14 堂組合包方案`
 
+INSERT INTO 
+	"CREDIT_PURCHASE" ("user_id", "credit_package_id", "purchased_credits", "price_paid")
+VALUES
+    (
+        (SELECT "id" FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io'),
+        (SELECT "id" FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
+        (SELECT "credit_amount" FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
+        (SELECT "price" FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案')
+    ),
+	(
+        (SELECT "id" FROM "USER" WHERE "email" = 'wXlTq@hexschooltest.io'),
+        (SELECT "id" FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案'),
+        (SELECT "credit_amount" FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案'),
+        (SELECT "price" FROM "CREDIT_PACKAGE" WHERE "name" = '21 堂組合包方案')
+    ),
+    (
+        (SELECT "id" FROM "USER" WHERE "email" = 'richman@hexschooltest.io'),
+        (SELECT "id" FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
+        (SELECT "credit_amount" FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案'),
+        (SELECT "price" FROM "CREDIT_PACKAGE" WHERE "name" = '14 堂組合包方案')
+    );
 
 -- ████████  █████   █    ████   
 --   █ █   ██    █  █         ██ 
@@ -53,6 +106,13 @@
     -- 1. 將用戶`李燕容`新增為教練，並且年資設定為2年（提示：使用`李燕容`的email ，取得 `李燕容` 的 `id` ）
     -- 2. 將用戶`肌肉棒子`新增為教練，並且年資設定為2年
     -- 3. 將用戶`Q太郎`新增為教練，並且年資設定為2年
+
+INSERT INTO
+	"COACH" ("user_id", "experience_years")
+VALUES
+	((SELECT "id" FROM "USER" WHERE "email" = 'lee2000@hexschooltest.io'), 2),
+	((SELECT "id" FROM "USER" WHERE "email" = 'muscle@hexschooltest.io'), 2),
+	((SELECT "id" FROM "USER" WHERE "email" = 'richman@hexschooltest.io'), 2);
 
 -- 3-2. 新增：承1，為三名教練新增專長資料至 `COACH_LINK_SKILL` ，資料需求如下：
     -- 1. 所有教練都有 `重訓` 專長
