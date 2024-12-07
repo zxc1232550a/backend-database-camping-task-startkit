0ae3e78666473d039a56dd8e70559d41a7b1ea9d
py_tsai <py_tsai@curdr.com>
2024-12-07 17:34:40 +0800

    finshed task4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -182,6 +182,25 @@ DELETE FROM "SKILL" WHERE "name" = '空中瑜伽';
     -- 6. 最大授課人數`max_participants` 設定為10
     -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
 
+INSERT INTO "COURSE" (
+    "user_id", 
+    "skill_id", 
+    "name", 
+    "start_at", 
+    "end_at", 
+    "max_participants", 
+    "meeting_url"
+)
+VALUES (
+    (SELECT "id" FROM "USER" WHERE "email" = 'lee2000@hexschooltest.io'), 
+    (SELECT "id" FROM "SKILL" WHERE "name" = '重訓'), 
+    '重訓基礎課', 
+    '2024-11-25 14:00:00', 
+    '2024-11-25 16:00:00', 
+    10, 
+    'https://test-meeting.test.io'
+);
+
 
 -- ████████  █████   █    █████ 
 --   █ █   ██    █  █     █     
